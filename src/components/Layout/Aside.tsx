51da4485a68cc5f1531dd60a9ed02604db3f9ed6
happy-func <stevenhanth@gmail.com>
2021-04-27 23:37:52 +0800

    侧边栏

--- src/components/Layout/Aside.tsx
+++ src/components/Layout/Aside.tsx
@@ -1,76 +1,79 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import { Divider, Drawer } from '@material-ui/core';
 import { connect } from 'react-redux';
-import { AppSlideBarState } from "@/store/action";
+import { AppSlideBarState, UpdateAppSlideBar } from "@/store/action";
 import styles from './aside.module.scss';
-import Menu from "@/components/Layout/Menu";
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: `nowrap`,
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create(`width`, {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create(`width`, {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: `hidden`,
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up(`sm`)]: {
-      width: theme.spacing(9) + 1,
-    },
-  },
-  toolbar: {
-    display: `flex`,
-    alignItems: `center`,
-    justifyContent: `flex-end`,
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-}));
+import { Menu } from "antd";
+import routes from "@/router/route";
+import { Dispatch } from "redux";
+import { useHistory } from "react-router-dom";
 
 interface Props {
   AppSlideBar: AppSlideBarState;
+  doUpdateAppSlideBar: UpdateAppSlideBar;
 }
 
-const Aside: React.FC<Props> = function({ AppSlideBar }) {
-  const classes = useStyles();
+const { SubMenu } = Menu;
 
+const Aside: React.FC<Props> = function({ AppSlideBar, doUpdateAppSlideBar }) {
+  const history = useHistory();
+  // @ts-ignore
+  function onMenuSelect(e) {
+    const { key } = e;
+    if (AppSlideBar.openKey !== key) {
+      doUpdateAppSlideBar({
+        ...AppSlideBar,
+        ...{
+          openKey: key,
+        },
+      });
+      const path = (function () {
+        for(const route of routes) {
+          let str = '';
+          if (route.name === key) {
+            return route.path;
+          } else if (route.children?.length) {
+            str = route.path;
+            for (const child of route.children) {
+              if (child.name === key) {
+                return str + child.path;
+              }
+            }
+          }
+        }
+        return '/dashBord';
+      })();
+      history.push(path);
+    } else {
+      doUpdateAppSlideBar({
+        ...AppSlideBar,
+        ...{
+          openKey: ``,
+        },
+      });
+    }
+  }
   return (
-    <Drawer
-      variant="permanent"
-      className={clsx(classes.drawer, {
-        [classes.drawerOpen]: AppSlideBar.openDrawer,
-        [classes.drawerClose]: !AppSlideBar.openDrawer,
-      })}
-      classes={{
-        paper: clsx({
-          [classes.drawerOpen]: AppSlideBar.openDrawer,
-          [classes.drawerClose]: !AppSlideBar.openDrawer,
-        }),
-      }}
+    <div
+      style={{ width: 240 }}
     >
-      <div className={classes.toolbar}>
-        {AppSlideBar.openDrawer && <div className={styles.title}>管理后台</div>}
-      </div>
-      <Divider />
-      <Menu />
-      <Divider />
-    </Drawer>
+      <Menu
+        theme="dark"
+        className={styles.menu}
+        mode="inline"
+        defaultSelectedKeys={['DashBord']}
+        onSelect={onMenuSelect}
+        inlineCollapsed={!AppSlideBar.openDrawer}
+      >
+        {routes.map((route) => (route.children?.length ? (
+          <SubMenu title={route.meta.title} key={route.name}>
+            {route.children.map((child) => (
+              <Menu.Item key={child.name}>{child.meta.title}</Menu.Item>
+            ))}
+          </SubMenu>
+        ) : <Menu.Item key={route.name}>{route.meta.title}</Menu.Item>)
+        )}
+      </Menu>
+    </div>
   )
 };
 
@@ -79,4 +82,9 @@ const mapStateToProps = (state: { AppSlideBar: any; }) => ({
   AppSlideBar: state.AppSlideBar,
 });
 
-export default connect(mapStateToProps)(Aside);
+// 将 对应action 插入到组件的 props 中
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  doUpdateAppSlideBar: (appSlideBarState: AppSlideBarState) => dispatch(UpdateAppSlideBar(appSlideBarState)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Aside);
