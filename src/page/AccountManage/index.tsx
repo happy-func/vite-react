bf1ef71cfa6b4575910b20af60e74dd024515da0
happy-func <stevenhanth@gmail.com>
2021-04-30 08:50:19 +0800

    长列表性能优化

--- src/page/AccountManage/index.tsx
+++ src/page/AccountManage/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { FixedSizeList } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
-const AccountManagePage: React.FC = () => (
-  <div>
-    <div>账号管理</div>
-    {[... new Array(10000).keys()].map((item) => <div key={item}>账号管理{item}</div>)}
-  </div>
-);
+const AccountManagePage: React.FC = () => {
+  // @ts-ignore
+  const Row = ({ index, style }) => (
+    <div style={style}>Row {index}</div>
+  );
+  return (
+    <div style={{ height: '100vh' }}>
+      <div>账号管理</div>
+      <AutoSizer>
+        {({ width, height }) => (
+          <FixedSizeList itemSize={35} height={height} itemCount={1000} width={width}>
+            {Row}
+          </FixedSizeList>
+        )}
+      </AutoSizer>
+    </div>
+  )
+};
 
 export default AccountManagePage;
