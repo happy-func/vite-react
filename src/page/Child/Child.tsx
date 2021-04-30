7092e5690a01ecd4663aeb0b4eb30cecb9185416
happy-func <stevenhanth@gmail.com>
2021-05-01 07:12:52 +0800

    文章编辑

--- src/page/Child/Child.tsx
+++ src/page/Child/Child.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import E from 'wangeditor';
 
 const Child = function() {
+  let editor: E;
+  useEffect(function () {
+    editor = new E('#editor');
+    editor.create();
+    return () => {
+      editor.destroy();
+    }
+  }, []);
+
+  // 获取html
+  function getHtml() {
+    alert(editor.txt.html())
+  }
+
+  // 获取text
+  function getText() {
+    alert(editor.txt.text())
+  }
   return (
-    <div>Child</div>
+    <div>
+      <div>Child</div>
+      <button onClick={getHtml}>获取html</button>
+      <button onClick={getText}>获取text</button>
+      <div id="editor" />
+    </div>
   );
 };
 
