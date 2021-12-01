705f3dad0ae1c45a1dd361a5341f6cd507051a03
zzx <zhaoxiang.zhang@surpath.net>
2021-12-01 14:57:53 +0800

    add google font

--- src/page/Child/Deep.tsx
+++ src/page/Child/Deep.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Icon from "@material-ui/core/Icon";
+import {Box, Button} from "@material-ui/core";
 
 const Deep: React.FC = function () {
-  return (<div>deep child</div>);
+  return (
+    <div>
+      <h1>deep child</h1>
+      <Box>
+        <Button>
+          <Icon>face</Icon>
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
 export default Deep;
