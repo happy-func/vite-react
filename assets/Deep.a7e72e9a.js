0065a50990d7af986815a536bf3467ab5022183d
happy-func <stevenhanth@gmail.com>
2021-12-09 23:36:11 +0800

    deploy

--- assets/Deep.a7e72e9a.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./vendor.3c7e9b6e.js";export default function(){return e.createElement("div",null,"deep child")}
---
+++ assets/Deep.c0cae36d.js
@@ -0,0 +1,1 @@
+import{R as e,B as n,n as t,I as l}from"./vendor.3ff8e5a0.js";const c=function(){return e.createElement("div",null,e.createElement("h1",null,"deep child"),e.createElement(n,null,e.createElement(t,null,e.createElement(l,null,"face"))))};export{c as default};
